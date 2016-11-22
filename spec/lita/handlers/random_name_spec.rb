be49ac4a811d78b30c4b359eb1eb9dfdaefd236b
gouf <innocent.zero@gmail.com>
2016-11-22 22:23:06 +0900

    Add rspec test

--- spec/lita/handlers/random_name_spec.rb
+++ spec/lita/handlers/random_name_spec.rb
@@ -1,4 +1,13 @@
 require "spec_helper"
 
 describe Lita::Handlers::RandomName, lita_handler: true do
+  it { is_expected.to route('randomname') }
+  it { is_expected.to route('random_name') }
+  it { is_expected.to route('randomname').to(:random_name) }
+  it { is_expected.to route('random_name').to(:random_name) }
+  it 'returns random name' do
+    test_regex = /[A-Za-z\-0-9]+/
+    send_message('random_name')
+    expect(replies.last).to match(test_regex)
+  end
 end
