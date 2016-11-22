bfb245adcd8026a4e67ed68a0ebfc74875e0abde
gouf <innocent.zero@gmail.com>
2016-11-22 22:24:07 +0900

    Add Randgen random name response

--- lib/lita/handlers/random_name.rb
+++ lib/lita/handlers/random_name.rb
@@ -1,7 +1,22 @@
+require 'randexp'
+require 'faker'
+require 'forgery'
+
+
+class Randgen
+  def self.rand_name(options = {})
+    /#{Forgery::Basic.color}-#{Faker::Name.first_name}-\d{4}/.gen.downcase
+  end
+end
+
 module Lita
   module Handlers
     class RandomName < Handler
-      # insert handler code here
+      route(/random[_]?name/, :random_name)
+
+      def random_name(response)
+        response.reply /[:rand_name:]/.gen
+      end
 
       Lita.register_handler(self)
     end
